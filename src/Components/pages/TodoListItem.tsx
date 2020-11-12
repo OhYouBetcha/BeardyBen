d6ed178baf6078a1a4b854b09b828c05e4981fd7
Ben Petersen <48611421+bdpetersen77@users.noreply.github.com>
2020-11-11 19:46:19 -0700

    Finished the Todo app

--- src/Components/pages/TodoListItem.tsx
+++ src/Components/pages/TodoListItem.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import '../../CSS/TodoListItem.css';
 
 //Todo is from the types.ts file
 interface TodoListItemProps {
   todo: Todo;
+  toggleTodo: ToggleTodo;
 }
 
-export const TodoListItem: React.FC<TodoListItemProps> = ({ todo }) => {
+export const TodoListItem: React.FC<TodoListItemProps> = ({
+  todo,
+  toggleTodo,
+}) => {
   return (
     <li>
-      <label>
-        {' '}
-        <input type="checkbox" checked={todo.complete}></input>
+      {/* IF todo is complete, then the classname is complete. else, it is undefined meaning no style.  */}
+      <label className={todo.complete ? 'complete' : undefined}>
+        <input
+          type="checkbox"
+          checked={todo.complete}
+          onChange={() => toggleTodo(todo)}
+        />
+        {todo.text}
       </label>
     </li>
   );
--- src/Components/pages/todoApp.tsx
+++ src/Components/pages/todoApp.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 /** @jsxImportSource @emotion/core */
 import { css, jsx } from '@emotion/core';
-import Switch from '@material-ui/core/Switch';
 import { FC, useState } from 'react';
-import TodoListItem from './TodoListItem';
+import { TodoList } from './TodoList';
+import { AddTodoForm } from './AddTodoForm';
 
+// HERE IS THE YOUTUBE VIDEO USED FOR THE TODO APP
+// https://www.youtube.com/watch?v=ODvirqIC09A&amp=&ab_channel=DevTuts
 //interface
 interface Props {
   title: string;
@@ -13,13 +15,36 @@ interface Props {
 
 //this basically says that this is a const var "todos" OF TYPE ARRAY
 //with the TYPE of "TODO" which is brought in from the TYPES file in types.d.ts
-const todos: Array<Todo> = [
+const initialTodos: Array<Todo> = [
   { text: 'Walk the dog', complete: true },
   { text: 'Write app', complete: false },
 ];
 
 ///what is actually exported,this is the base function
 export const ToDoApp: FC<Props> = ({ title }) => {
+  //In order to manage the checked/unchecked status we need to manage state
+  const [todos, setTodos] = useState(initialTodos);
+
+  //function to toggle the todo's
+  const toggleTodo: ToggleTodo = (selectedTodo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  //handler
+  const addTodo: AddTodo = (newTodo) => {
+    newTodo.trim() !== '' &&
+      setTodos([...todos, { text: newTodo, complete: false }]);
+  };
+
   return (
     <div
       css={css`
@@ -44,7 +69,10 @@ export const ToDoApp: FC<Props> = ({ title }) => {
 
           {/* TODO LIST START */}
           <div>
-            <TodoListItem />
+            <React.Fragment>
+              <TodoList todos={todos} toggleTodo={toggleTodo} />
+              <AddTodoForm addTodo={addTodo} />
+            </React.Fragment>
           </div>
         </Paper>
       </div>
